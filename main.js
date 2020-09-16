003e40aa40199e81220dcba25d17b355207179a0
Eric Dong <44481166+not-ericdong@users.noreply.github.com>
2020-09-15 23:22:50 -0400

    added more pages fixed css

---
+++ main.js
@@ -0,0 +1,31 @@
+let input = document.querySelector("input");
+let container = document.querySelector('.container')
+
+let submitButton = document.querySelector('button');
+let buttonText = documnet.createTextNode('Submit');
+
+
+
+// let name = 'a';
+// name = "lol";
+// console.log(name);
+// const cantChangeName = "me";
+// // cant change above variables
+
+
+// let person = {
+//    name: "me",
+//    age: 30
+// }
+
+// person.name
+// person['name']
+
+// let selectedcolor = ["red", 'blue'];
+// selectedcolor[2] = 1;
+// console.log(selectedcolor.length);
+
+// function greet() {
+//    console.log("hello");
+// }
+// greet();
