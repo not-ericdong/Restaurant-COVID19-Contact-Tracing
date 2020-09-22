f4a7d1c134f9c158a9aae03497d3b8aeb8c37df4
Eric Dong <44481166+not-ericdong@users.noreply.github.com>
2020-09-21 20:05:51 -0400

    added connections and CRUD to dynamodb

--- main.js
+++ main.js
@@ -1,31 +1,112 @@
-let input = document.querySelector("input");
-let container = document.querySelector('.container')
+/* ----------- email validation --------------- */
+function ValidateEmail(emailInput)
+{
 
-let submitButton = document.querySelector('button');
-let buttonText = documnet.createTextNode('Submit');
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (emailInput.value.match(mailformat)) {
+        alert("Thanks for entering an email address!");
+        document.form1.text1.focus();
+        return (true);
+    } else {
+        // let errorMsgP = document.createElement("div");
+        // let errorMsg = document.createTextNode("You have entered an invalid email address!");
+        // errorMsgP.appendChild(errorMsg);
+        // let currentDiv = document.getElementById("button1");
+        // document.body.insertBefore(errorMsgP, currentDiv);
+        alert("Please enter a valid email address!");
+        document.form1.text1.focus();
+        return (false);
+    }
+}
+
+/* Date time stuff */
+var d = new Date(); //current time
+var dateTime = d.getTime();
+// 2hr = 7200000ms
+
+/* -----------------aws dynamoDB-------------------- */
+/* read text files */
 
 
 
-// let name = 'a';
-// name = "lol";
-// console.log(name);
-// const cantChangeName = "me";
-// // cant change above variables
+AWS.config.update({
+    region: "us-east-2",
+    endpoint: "dynamodb.us-east-2.amazonaws.com",
+    accessKeyId: "",
+    secretAccessKey: ""
+ });
+
+var dynamoDB = new AWS.DynamoDB();
+
+/* create, read, delete item in db */
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+function createItem(email_text, location) {
+   var params = {
+       TableName :"User-Data",
+       Item:{
+            "Email": email_text,
+            "Date": dateTime,
+            "Location": location,
+            "LocationName": "location name"
+       }
+   };
+   docClient.put(params, function(err, data) {
+       if (err) {
+           document.getElementById('textarea').innerHTML = "Unable to add item: " + "\n" + JSON.stringify(err, undefined, 2);
+       } else {
+           document.getElementById('textarea').innerHTML = "PutItem succeeded: " + "\n" + JSON.stringify(data, undefined, 2);
+       }
+   });
+}
+
+function readItem(email_text) {
+   var table = "User-Data";
+   var email = email_text;
+   var date = 123;
+   var info = {};
 
 
-// let person = {
-//    name: "me",
-//    age: 30
-// }
+   var params = {
+       TableName: table,
+       Key:{
+           "Email": email,
+        //    "Date": date,
+            "Location": "The Food Place"
+       }
+   };
+   docClient.get(params, function(err, data) {
+       if (err) {
+           document.getElementById('textarea').innerHTML = "Unable to read item: " + "\n" + JSON.stringify(err, undefined, 2);
+       } else {
+           document.getElementById('textarea').innerHTML = "GetItem succeeded: " + "\n" + JSON.stringify(data, undefined, 2);
+       }
+   });
+}
 
-// person.name
-// person['name']
+function conditionalDelete(email_text) {
+   var table = "User-Data";
+   var email = email_text;
+   var date = 2020;
 
-// let selectedcolor = ["red", 'blue'];
-// selectedcolor[2] = 1;
-// console.log(selectedcolor.length);
+   var params = {
+       TableName:table,
+       Key:{
+           "year":year,
+           "title":title
+       },
+       ConditionExpression:"info.rating <= :val",
+       ExpressionAttributeValues: {
+           ":val": 5.0
+       }
+   };
 
-// function greet() {
-//    console.log("hello");
-// }
-// greet();
+   docClient.delete(params, function(err, data) {
+       if (err) {
+           document.getElementById('textarea').innerHTML = "The conditional delete failed: " + "\n" + JSON.stringify(err, undefined, 2);
+       } else {
+           document.getElementById('textarea').innerHTML = "The conditional delete succeeded: " + "\n" + JSON.stringify(data, undefined, 2);
+       }
+   });
+}
---
+++ url.js
@@ -0,0 +1,17 @@
+/* random urls */
+Math.floor(Math.random());
+let root_path = 'file:///C:/Users/moond/PycharmProjects/Restaurant-COVID19-Contact-Tracing/'
+let url1 = new URL('/index.html', root_path)
+
+/* QR code generator */
+var qrcode = new QRCode("qrcode");
+
+function makeQR () {
+   var locText = document.getElementById("qrData");
+   if (!locText.value) {
+      alert("Please enter a location");
+      locText.focus();
+      return;
+   }
+   qrcode.makeCode(locText.value);
+}
