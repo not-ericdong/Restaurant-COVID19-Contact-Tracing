ac35a0548bedb16c78f8da4e4598f8e7ccd6c9a6
Eric Dong <44481166+not-ericdong@users.noreply.github.com>
2020-09-22 15:18:18 -0400

    better css

--- url.js
+++ url.js
@@ -3,6 +3,10 @@ Math.floor(Math.random());
 let root_path = 'file:///C:/Users/moond/PycharmProjects/Restaurant-COVID19-Contact-Tracing/'
 let url1 = new URL('/index.html', root_path)
 
+
+
+
+
 /* QR code generator */
 var qrcode = new QRCode("qrcode");
 
